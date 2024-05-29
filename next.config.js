06e1b9e066f294fa1b51ebd8553a8f6344527e18
Govind783 <govindganeriwal@gmail.com>
2024-05-30 01:02:56 +0530

    env

--- next.config.js
+++ next.config.js
@@ -14,6 +14,17 @@ const nextConfig = {
 
     return config;
   },
+  env: {
+    AUTH0_SECRET: process.env.AUTH0_SECRET,
+    AUTH0_BASE_URL: process.env.AUTH0_BASE_URL,
+    AUTH0_ISSUER_BASE_URL: process.env.AUTH0_ISSUER_BASE_URL,
+    AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
+    AUTH0_CLIENT_SECRET: process.env.AUTH0_CLIENT_SECRET,
+    BACKEND_URL: process.env.BACKEND_URL,
+    LIVEKIT_API_KEY: process.env.LIVEKIT_API_KEY,
+    LIVEKIT_API_SECRET: process.env.LIVEKIT_API_SECRET,
+    NEXT_PUBLIC_LIVEKIT_URL: process.env.NEXT_PUBLIC_LIVEKIT_URL,
+  },
 }
 
 module.exports = nextConfig
